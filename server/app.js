10fbd99dc6626f45523d96942e528c5a04927dbc
Ankit <ankitsharma.p413@gmail.com>
2026-05-11 15:48:20 +0530

    app.js cors updated

--- server/app.js
+++ server/app.js
@@ -13,7 +13,9 @@ const PORT  = process.env.PORT || 4000;
 
 const app = e();
 
-app.use(cors());
+app.use(cors({
+    origin:['http://localhost:5173','https://artify-client-3vez.onrender.com']
+}));
 app.use(e.json());
 
 // routes
